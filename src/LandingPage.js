9a6f96c1f1a24263a4546cf5262c4e2f34de65d4
coastalcdr <105960308+coastalcdr@users.noreply.github.com>
2024-08-20 02:28:14 -0400

    Added LandingPage component with background image and Get Started button

---
+++ src/LandingPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './LandingPage.css'; 
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="background-image"></div>
+      <div className="content">
+        <h1>Paradise Nursery</h1>
+        <p>Welcome to Paradise Nursery</p>
+        <Link to="/products" className="get-started-button">Get Started</Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
